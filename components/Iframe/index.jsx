ac80e12a6b7d955023d29ce4a0ef1bdb051106d5
Manish Dalal <manish.dalal@moonshinetechnology.com>
2022-08-10 22:59:17 +0530

    changes

--- components/Iframe/index.jsx
+++ components/Iframe/index.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import style from './index.module.css';
 
 const IframeRender = (props) => {
+  const [dimensions, setdimensions] = useState({ height: 100, width: 320 });
+
+  const setDimensions = (params) => setdimensions({ ...dimensions, ...params });
+  React.useEffect(() => {
+    setDimensions({ width: window.innerWidth, height: window.innerHeight });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className={style.customiframebox}>
       <iframe
         src={props.static}
         title='name'
-        height={props.height}
-        width={props.width}
+        height={props.height || dimensions.height}
+        width={props.width || dimensions.width}
         frameBorder={0}
         sandbox='allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation '
       />
