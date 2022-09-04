f2bb9501dd89f15647ca0e5d7ede62d59eb45ab0
Manish Dalal <manish.dalal@moonshinetechnology.com>
2022-09-04 16:04:29 +0530

    changes

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a className={styles.link} href='/term-and-condition'>
-        Terms of Use
-      </a>
-      <a className={styles.link} href='/privacy-policy'>
-        Privacy Policy
-      </a>
-      <a className={styles.link} href='/disclaimer'>
-        Disclaimer
-      </a>
-      <a className={styles.link} href='/contact'>
-        Contact
-      </a>
+      <span className={styles.link}>
+        <Link href='/term-and-condition'>Terms of Use</Link>
+      </span>
+      <span className={styles.link}>
+        <Link href='/privacy-policy'>Privacy Policy</Link>
+      </span>
+      <span className={styles.link}>
+        <Link href='/disclaimer'>Disclaimer</Link>
+      </span>
+      <span className={styles.link}>
+        <Link href='/contact'>Contact</Link>
+      </span>
     </footer>
   );
 }
