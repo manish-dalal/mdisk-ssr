a2f8622b71c508aca2a0aafb64800f1c45c1bcf7
Manish Dalal <manish.dalal@moonshinetechnology.com>
2022-12-12 18:38:52 +0530

    changes

--- components/FooterAds/FooterAds.jsx
+++ components/FooterAds/FooterAds.jsx
@@ -2,11 +2,8 @@ import React, { useEffect, useState, useRef } from 'react';
 import styles from './FooterAds.module.css';
 import { getMobileOS } from '../../utils/get-mobile-os';
 
-// let adPageSize = getMobileOS() === 'Android' ? 10 : 1;
-// let loadCount = getMobileOS() === 'Android' ? 1000 : 1;
-
-let adPageSize = 1;
-let loadCount = 1;
+let adPageSize = getMobileOS() === 'Android' ? 10 : 1;
+let loadCount = getMobileOS() === 'Android' ? 1000 : 1;
 
 export default function MdiskInfo({ isLoading, exoAdsArr }) {
   const [count, setcount] = useState(adPageSize);
